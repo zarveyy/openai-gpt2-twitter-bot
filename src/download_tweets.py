cdb824c031a50140a87b319844ea02b50888e743
Axel Boissier <axel.bssier@gmail.com>
2020-11-09 23:03:34 +0100

    first commit

--- src/download_tweets.py
+++ src/download_tweets.py
@@ -1,14 +1,10 @@
 import tweepy
+import twitter_credentials as tc
 import random
 import time
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(tc.consumer_key, tc.consumer_secret)
+auth.set_access_token(tc.access_token, tc.access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
@@ -18,7 +14,7 @@ def download_all_tweets(user):
     for page in tweepy.Cursor(api.user_timeline, screen_name=user, tweet_mode='extended').pages():
         page_list.append(page)
 
-    with open("downloaded_tweets.txt","a",encoding="utf-8") as out_file:
+    with open("downloaded_tweets.csv","a",encoding="utf-8") as out_file:
         for page in page_list:
             for status in page:
                 if not status.retweeted:
@@ -26,5 +22,5 @@ def download_all_tweets(user):
                     print(status.full_text)
 
 if __name__ == "__main__":
-    for user in [""]:
+    for user in ["TwitterUsername"]:
         download_all_tweets(user)
